ea6bab624fb6fef61379c4af00011bceeea578cb
Iszzmail <78975484+Iszzmail@users.noreply.github.com>
2021-07-09 19:57:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+// //Import react and react DOM
+
+// import React from 'react'   
+// import ReactDOM from 'react-dom'
+// import CommentSection from './Commentdetail';
+// import faker from 'faker'
+// import ApprovalCard  from './ApprovalCard';
+
+// //Create a react component
+// const App =()=>{
+//     return (
+//       <div className="ui container comments">
+//          <ApprovalCard> 
+//         <CommentSection
+//           author="ismail"
+//           timeAgo="Today @ 5:00PM"
+//           comment="Great Post"
+//           image={faker.image.avatar()}
+//         />
+//         </ApprovalCard>
+//         <ApprovalCard>
+//         <CommentSection
+//           author="Mani"
+//           timeAgo="Today @ 9:30PM"
+//           comment="Simple Post"
+//           image={faker.image.avatar()}
+//         />
+//         </ApprovalCard>
+
+//         <ApprovalCard>
+//         <CommentSection
+//           author="Ammer"
+//           timeAgo="Sterday @ 7:00AM"
+//           comment="Cringe Post"
+//           image={faker.image.avatar()}
+//         />
+//         </ApprovalCard>
+//       </div>
+//     );
+//     }
+// //Show the component on the screen
+// ReactDOM.render(
+//     <App/>,
+//     document.querySelector('#root')
+// )
